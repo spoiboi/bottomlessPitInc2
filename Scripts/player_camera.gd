24dd494b285778398d930de0d638982deef48e92
manspider-thatdev <peterson.parker.james@gmail.com>
2024-02-26 14:10:53 -0600

    Bug Fix with camera

--- Scripts/player_camera.gd
+++ Scripts/player_camera.gd
@@ -2,25 +2,25 @@ extends Camera3D
 
 
 @export var radius := 8.0
-@export_range(0, 360) var theta := 0.0:
+@export_range(0, 360) var theta := 90.0:
 	get:
 		return deg_to_rad(theta)
 	set(value):
 		theta = wrapf(rad_to_deg(value), 0, 360)
-		set_camera_position()
-@export_range(0, 180) var phi := 90.0:
+@export_range(1, 179) var phi := 90.0:
 	get:
 		return deg_to_rad(phi)
 	set(value):
-		phi = clampf(rad_to_deg(value), 0, 180)
-		set_camera_position()
+		phi = clampf(rad_to_deg(value), 1.0, 179.0)
 
 
 func set_camera_position():
 	position = radius * Vector3(sin(phi) * cos(theta), cos(phi), sin(phi) * sin(theta))
-	look_at(get_parent().global_position)
+	#rotation = Vector3(0, -theta + 90, 0)
+	look_at(get_parent_node_3d().global_position)
 
 
 func _process(delta):
 	theta += Input.get_axis("camera_right", "camera_left") * delta
 	phi += Input.get_axis("camera_up", "camera_down") * delta
+	set_camera_position()
