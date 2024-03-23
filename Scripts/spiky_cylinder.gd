115068be2ab8da8807b693b579733c09633f438e
manspider-thatdev <peterson.parker.james@gmail.com>
2024-03-23 11:33:32 -0500

    Added Moving Platform

--- Scripts/spiky_cylinder.gd
+++ Scripts/spiky_cylinder.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
 
-var rotation_speed
+@export var rotation_speed = 6.0
+
+
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$SpikyCylinder.rotation.x += 1
+	rotate_object_local(Vector3.RIGHT, delta * rotation_speed)
