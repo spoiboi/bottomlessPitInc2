ec516b968562b436b45b60ea356ead03d3b6694c
owenmiester05 <ofaddison@gmail.com>
2024-03-23 11:35:51 -0500

    added blade platform

--- Scripts/blade.gd
+++ Scripts/blade.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
-@export var spinSpeed = .1
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_object_local(Vector3.RIGHT, delta * spinSpeed)
+	rotate_object_local(Vector3(0, 1, 0), delta)
