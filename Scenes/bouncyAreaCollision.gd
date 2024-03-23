2f87db2e6dc714219052c3ddc47e388a1e035709
WildkrattFan <cosmiccollectors131@gmail.com>
2024-03-23 11:07:32 -0500

    spring work

---
+++ Scenes/bouncyAreaCollision.gd
@@ -0,0 +1,16 @@
+extends CollisionShape3D
+
+@export var bounce = 400
+var player
+var areaBouncy
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_parent().get_node("Player")
+	areaBouncy = get_node("bouncyArea") #Area2D is the name of your area
+	areaBouncy.connect("body_enter", player,"_on_bouncy_body_enter")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- bouncy_area.gd
+++ bouncy_area.gd
@@ -1,11 +1,11 @@
-extends Node3D
+extends CollisionShape3D
 
 
-var rotation_speed
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$SpikyCylinder.rotation.x += 1
+	pass
