db86fc9f57ea2a2b3c5b300eccc87e8e0d405ce9
owenmiester05 <ofaddison@gmail.com>
2024-04-13 11:12:26 -0500

    added spiky cyclinder

--- Scripts/spiky_cylinder.gd
+++ Scripts/spiky_cylinder.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
-
+func respawn():
+	pass
+	
 func _ready():
 	pass # Replace with function body.
 
@@ -8,3 +10,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotate_object_local(Vector3.RIGHT, 0.1)
+	
+
+
+func _on_static_body_3d_body_entered(body):
+	body.respawn()
+
+
