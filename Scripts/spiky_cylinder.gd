4a6997f4a4e076ed741ffc76792384bb416abfa7
manspider-thatdev <peterson.parker.james@gmail.com>
2024-04-20 10:30:12 -0500

    Fixed spiky cylinder collision
    Duuuuuuuude physics process is funny

--- Scripts/spiky_cylinder.gd
+++ Scripts/spiky_cylinder.gd
@@ -1,20 +1,13 @@
 extends Node3D
 
+
 func respawn():
 	pass
-	
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previouds frame.
-func _process(delta):
-	rotate_object_local(Vector3.RIGHT, 0.01)
-	
+func _physics_process(delta):
+	rotate_object_local(Vector3.RIGHT, delta)
 
 
 func _on_static_body_3d_body_entered(body):
-	print("Died to cylinder!")
 	body.respawn()
-
-
