6d36e6d41bcd09b2b3acc2e2e176b0d66f095f21
spoiboi <ntbearden@gmail.com>
2024-04-30 15:05:43 -0500

    fixed crumbling platform

--- Scripts/crumbling_platform.gd
+++ Scripts/crumbling_platform.gd
@@ -1,29 +1,21 @@
 extends Node3D
 
-@export var default_height = 0
-@export var crumble_velocity = 0
-
-@export var crumbling = false
+var time = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	default_height = position.y
+	set_process(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ($checker.get_collision_count() > 0) or crumbling:
-		crumble()
-	
-
-func crumble():
-	position.y -= crumble_velocity
-	crumble_velocity +=0.1
-	if position.y < (default_height - 20):
-		position.y = default_height
-		crumble_velocity = 0
-		crumbling = false
-
-#func start_timer(start_delta, current_delta):
-		#
+	time += 1
 		
+
+
+func _on_area_3d_body_entered(body):
+	$Timer.start(0.7)
+
+
+func _on_timer_timeout():
+	queue_free()
