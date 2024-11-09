04d1122db5b819e459f959ea87276dc483adc117
TheGooseMafiaForReal <gavinjshaner@gmail.com>
2024-11-09 11:18:02 -0600

    Helllllllllll yeah

--- Levels/button_level.gd
+++ Levels/button_level.gd
@@ -12,12 +12,35 @@ func _process(delta: float) -> void:
 	$Brother.turning_off.connect(_toggle_off)
 	
 func _toggle_on():
-	$OnPlatform._toggle_on()
-	$OffPlatform._restore_off()
+	$OnPlatform2._toggle_on()
+	$OnPlatform3._toggle_on()
+	$OnPlatform4._toggle_on()
+	$OnPlatform5._toggle_on()
+	$OnPlatform6._toggle_on()
+	$OnPlatform7._toggle_on()
+	$OnPlatform8._toggle_on()
+	$OnPlatform9._toggle_on()
+	$OffPlatform2._restore_off()
+	$OffPlatform3._restore_off()
+	$OffPlatform4._restore_off()
+	$OffPlatform5._restore_off()
+	$OffPlatform6._restore_off()
+	
 	
 func _toggle_off():
-	$OffPlatform._toggle_off()
-	$OnPlatform._restore_on()
+	$OffPlatform2._toggle_off()
+	$OffPlatform3._toggle_off()
+	$OffPlatform4._toggle_off()
+	$OffPlatform5._toggle_off()
+	$OffPlatform6._toggle_off()
+	$OnPlatform2._restore_on()
+	$OnPlatform3._restore_on()
+	$OnPlatform4._restore_on()
+	$OnPlatform5._restore_on()
+	$OnPlatform6._restore_on()
+	$OnPlatform7._restore_on()
+	$OnPlatform8._restore_on()
+	$OnPlatform9._restore_on()
 
 
 	
