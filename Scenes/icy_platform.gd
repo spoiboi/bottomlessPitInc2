aecd1ce1bb435335da56b0bee09b597fa575df96
owenmiester05 <ofaddison@gmail.com>
2024-11-09 22:09:21 -0600

    added hard level

--- Scenes/icy_platform.gd
+++ Scenes/icy_platform.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
-func is_ice():
-	return true
+
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Scripts/ice.gd
+++ Scripts/ice.gd
@@ -1,16 +1,44 @@
 extends Area3D
 
-
 signal on_ice_platform(speed_multiplier)
 
 # This multiplier can be adjusted to control the "slippery" effect.
 const ICE_SPEED_MULTIPLIER = 0.5
+var player 
+func _ready() -> void:
+	print("ready")
+func _on_Area3D_body_entered(body):
+	print("hi")
+	if body.name == "Player":
+		player = body
+		
+		$Timer.start()
+
+func _on_Area3D_body_exited(body):
+	print("bye")
+	if body.name == "Player":
+		$Timer.stop()
+		
+func _on_timer_timeout():
+	# Check if the player has velocity and decrease it
+	if player and player.velocity.length() > 0:
+		# Decrease velocity by the decrement amount and apply multiplier
+		player.velocity *= .8
+		# Restart the timer for continuous slowing
+		$Timer.start()
+	
+	
+	
+
+		
+		
 
 # When character enters the ice platform
-func _on_Area3D_body_entered(body):
+"""func _on_Area3D_body_entered(body):
 	if body is CharacterBody3D:
 		emit_signal("on_ice_platform", ICE_SPEED_MULTIPLIER)
 
 func _on_Area3D_body_exited(body):
 	if body is CharacterBody3D:
 		emit_signal("on_ice_platform", 1.0) 
+"""
